35d5c2654266172978fee514f3e2b6404ff4ca4b
Eldho@1802 <eldhotbabu12@gmail.com>
2025-03-13 19:56:17 +0530

    Initial commit

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+//write basic express boilerplate code
+// with express.json(). miidleware
+
+const express = require('express')
+const app = express()
+
+
+app.use(express.jsoon());
+
+
+/*body {
+ title: string
+ description: string
+ 
+
+}
+*/
+app.post("/todo", function(req, res){
+
+})
+
+app.get("/todos", function(req, res){
+
+})
+
+app.put("/completed", function(req, res){
+
+})
---
+++ readme.md
@@ -0,0 +1,12 @@
+#TODO APP
+This project contains a sinple todo app:
+It has the following features -
+ - Anyone can create a todo
+ - Anyone can see existing todos
+ - Anyone can marl todos as done
+
+
+ // initialilze a node project
+ // put a package.json
+
+ 
