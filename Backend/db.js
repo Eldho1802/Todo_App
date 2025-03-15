bab02368525dbe2d70da3b63f6088b890b699536
Eldho@1802 <eldhotbabu12@gmail.com>
2025-03-15 21:38:02 +0530

    first commit

---
+++ Backend/db.js
@@ -0,0 +1,24 @@
+/*
+* todo{
+title: string;
+description : string;
+}
+
+
+*/
+
+const mongoose = require("mongoose");
+
+mongoose.connect("")
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+
+const todo = mongoose.model('todos', todoSchema);
+module.exports = {
+    todo
+}
